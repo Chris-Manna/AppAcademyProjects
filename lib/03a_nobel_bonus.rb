a5e95344d7626643d5b632dca65faa53a8ed8358
James Amaya & Christopher Manna <jameseamaya@gmail.com & cmanna285@gmail.com>
2017-03-13 16:03:45 -0400

    Complete exercises up to 06

--- lib/03a_nobel_bonus.rb
+++ lib/03a_nobel_bonus.rb
@@ -15,6 +15,10 @@ def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
   SELECT
-  
+    yr
+  FROM
+    nobels
+  WHERE
+    subject = 'Physics' AND NOT subject = 'Chemistry'
   SQL
 end
